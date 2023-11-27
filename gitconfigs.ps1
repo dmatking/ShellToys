4c4a40e0c4d00d6410fdcb365fdc70e426b5e4da
David King <dmatking@yahoo.com>
2023-11-27 17:34:07 -0600

    adding stuff

---
+++ gitconfigs.ps1
@@ -0,0 +1,58 @@
+function Show-GitConfigs {
+    # Function to parse git config output into a hashtable
+    function Parse-GitConfig {
+        param ([string]$configLevel)
+        $config = @{}
+        git config $configLevel -l | ForEach-Object {
+            $key, $value = $_ -split '=', 2
+            $config[$key] = $value
+        }
+        return $config
+    }
+
+    # Collect configurations
+    $systemConfig = Parse-GitConfig --system
+    $globalConfig = Parse-GitConfig --global
+    $localConfig = Parse-GitConfig --local
+
+    # Display configurations with colors
+    Write-Host "System Configuration:" -ForegroundColor Yellow
+    $systemConfig.GetEnumerator() | Sort-Object Key | ForEach-Object { "    $($_.Key) = $($_.Value)" }
+    Write-Host ""
+
+    Write-Host "Global Configuration:" -ForegroundColor Green
+    $globalConfig.GetEnumerator() | Sort-Object Key | ForEach-Object { "    $($_.Key) = $($_.Value)" }
+    Write-Host ""
+
+    Write-Host "Local Configuration:" -ForegroundColor Cyan
+    $localConfig.GetEnumerator() | Sort-Object Key | ForEach-Object { "    $($_.Key) = $($_.Value)" }
+    Write-Host ""
+
+    # Determine effective configuration and source
+    $effectiveConfig = @{}
+    $systemConfig.GetEnumerator() | ForEach-Object { $effectiveConfig[$_.Key] = @{ Value=$_.Value; Source='System'; Overridden=$false } }
+    $globalConfig.GetEnumerator() | ForEach-Object { 
+        if ($effectiveConfig.ContainsKey($_.Key)) { $effectiveConfig[$_.Key].Overridden = $true }
+        $effectiveConfig[$_.Key] = @{ Value=$_.Value; Source='Global'; Overridden=$effectiveConfig[$_.Key].Overridden }
+    }
+    $localConfig.GetEnumerator() | ForEach-Object { 
+        if ($effectiveConfig.ContainsKey($_.Key)) { $effectiveConfig[$_.Key].Overridden = $true }
+        $effectiveConfig[$_.Key] = @{ Value=$_.Value; Source='Local'; Overridden=$effectiveConfig[$_.Key].Overridden }
+    }
+
+    # Display effective configuration with source-based color for overridden properties
+    Write-Host "Effective Configuration:" -ForegroundColor Magenta
+    $effectiveConfig.GetEnumerator() | Sort-Object Key | ForEach-Object {
+        $color = if ($_.Value.Overridden) {
+            switch ($_.Value.Source) {
+                'Local' { 'Cyan' }
+                'Global' { 'Green' }
+                'System' { 'Yellow' }
+            }
+        } else { 'White' }
+        
+        Write-Host "    $($_.Key) = $($_.Value.Value)" -ForegroundColor $color
+    }
+}
+
+Set-Alias -Name gitconfigs -Value Show-GitConfigs
