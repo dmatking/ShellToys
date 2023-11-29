9ad1512e31a96fc8b76de1ea5cb814570987bec5
David King <dmatking@yahoo.com>
2023-11-28 21:54:20 -0600

    working with color coding all effective lines with source and overridden lines in the source of overrides color

--- python/wip/gitconfigs.py
+++ python/wip/gitconfigs.py
@@ -30,22 +30,31 @@ def print_config(config, title, color_code):
 
 def determine_effective_config(system_config, global_config, local_config):
     effective_config = {}
-    source_of_override = {}
+    original_source = {}
+    override_source = {}
+
+    # Load system configuration
     for key, value in system_config.items():
         effective_config[key] = value
-        source_of_override[key] = 'system'
+        original_source[key] = 'system'
 
+    # Update with global configuration and track overrides
     for key, value in global_config.items():
-        if key not in effective_config or effective_config[key] != value:
-            source_of_override[key] = 'global'
+        if key in effective_config and effective_config[key] != value:
+            override_source[key] = 'global'
         effective_config[key] = value
+        if key not in original_source:
+            original_source[key] = 'global'
 
+    # Update with local configuration and track overrides
     for key, value in local_config.items():
-        if key not in effective_config or effective_config[key] != value:
-            source_of_override[key] = 'local'
+        if key in effective_config and effective_config[key] != value:
+            override_source[key] = 'local'
         effective_config[key] = value
+        if key not in original_source:
+            original_source[key] = 'local'
 
-    return effective_config, source_of_override
+    return effective_config, original_source, override_source
 
 def main():
     parser = argparse.ArgumentParser(description='Display Git configurations with override option')
@@ -74,13 +83,13 @@ def main():
     print_config(global_config, 'Global', COLORS['global'])
     print_config(local_config, 'Local', COLORS['local'])
 
-     # Determine effective configuration
-    effective_config, source_of_override = determine_effective_config(system_config, global_config, local_config)
+    # Determine effective configuration
+    effective_config, original_source, override_source = determine_effective_config(system_config, global_config, local_config)
 
     # Print effective configuration with color coding
     print('\033[35mEffective Configuration:\033[0m')  # Magenta
     for key, value in sorted(effective_config.items()):
-        source = source_of_override.get(key, 'default')
+        source = override_source.get(key, original_source.get(key, 'default'))
         color_code = COLORS.get(source, COLORS['default'])
         print(f'\033[{color_code}m    {key} = {value}\033[0m')
 
